2aa84bfb8fde061d14557fd70ec88065569d11c9
Your Name <alkamel.fares2@gmail.com>
2026-03-07 17:07:46 -0500

    control_flow

---
+++ controlFlow/control_flow.js
@@ -0,0 +1,44 @@
+let userRole="admin";
+let accessLevel;
+if(userRole === "admin"){
+    accessLevel="Full access granted";
+} else if (userRole === "manger"){
+    accessLevel ="Limited access granted";
+} else {
+    accessLevel="no access granted";
+}
+console.log("Access level is:", accessLevel);
+
+//Step 3
+let isLoggedIn = true;
+let userMessage;
+if(isLoggedIn === true){
+    if(userRole === "admin"){
+        userMessage="Welcome, Admin!";
+    }
+    else{
+        userMessage="WelCome, user!";
+    }
+}
+else{
+    userMessage="Please log in to access the system.";
+}
+console.log("User Message:", userMessage);
+
+//Step 4
+let userType="subscriber";
+let userCategory;
+switch(userType){
+    case "admin":
+        userCategory="admin";
+        break;
+    case "manager":
+        userCategory="Manager";
+        break;
+    case "subscriber":
+        userCategory = "Subscriber";
+        break; 
+    default:
+        userCategory="Unkown";
+}
+console.log("user catagory: ", userCategory);
